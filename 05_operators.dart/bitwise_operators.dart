7bfe34606b65e5ef8699d806ffd93db168dbca37
Khaled-Mahmoud1 <khaled.mahmoud.it@gmail.com>
2026-03-01 22:58:31 +0200

    Add bitwise operators

---
+++ 05_operators.dart/bitwise_operators.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 5; // 0101
+  int b = 3; // 0011
+
+  print("a & b = ${a & b}"); // AND
+  print("a | b = ${a | b}"); // OR
+  print("a ^ b = ${a ^ b}"); // XOR
+  print("~a = ${~a}");       // NOT
+  print("a << 1 = ${a << 1}"); // Shift Left
+  print("a >> 1 = ${a >> 1}"); // Shift Right
+}
