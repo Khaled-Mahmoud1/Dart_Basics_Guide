9f6b2b877de2017f207b94eb4f719e2981c0c98e
Khaled-Mahmoud1 <khaled.mahmoud.it@gmail.com>
2026-03-05 23:29:26 +0200

    Update student_management_system.dart

--- 16_practical_exercises_dart/student_management_system.dart
+++ 16_practical_exercises_dart/student_management_system.dart
@@ -20,10 +20,33 @@ void addStudent() {
   students.add({
     "name": name,
     "grades": [g1, g2, g3],
-    "average": avg
+    "average": avg,
   });
 
   print("Student Added");
+}
+
+String getGrade(double avg) {
+  if (avg >= 90) return "A";
+  if (avg >= 80) return "B";
+  if (avg >= 70) return "C";
+  if (avg >= 60) return "D";
+  return "F";
+}
+
+void deleteStudent() {
+  print("Enter student name to delete:");
+  String name = stdin.readLineSync()!;
+
+  int before = students.length;
+
+  students.removeWhere((s) => s["name"].toLowerCase() == name.toLowerCase());
+
+  if (students.length < before) {
+    print("Student deleted successfully.");
+  } else {
+    print("Student not found.");
+  }
 }
 
 void showStudents() {
@@ -33,9 +56,12 @@ void showStudents() {
   }
 
   for (var s in students) {
+    double avg = s["average"];
+
     print("\nName: ${s["name"]}");
     print("Grades: ${s["grades"]}");
-    print("Average: ${s["average"].toStringAsFixed(2)}");
+    print("Average: ${avg.toStringAsFixed(2)}");
+    print("Grade: ${getGrade(avg)}");
   }
 }
 
@@ -82,26 +108,35 @@ void main() {
     print("2. Show All Students");
     print("3. Search Student");
     print("4. Show Top Student");
-    print("5. Exit");
-
+    print("5. Delete Student");
+    print("6. Exit");
     String? choice = stdin.readLineSync();
 
     switch (choice) {
       case "1":
         addStudent();
         break;
+
       case "2":
         showStudents();
         break;
+
       case "3":
         searchStudent();
         break;
+
       case "4":
         topStudent();
         break;
+
       case "5":
+        deleteStudent();
+        break;
+
+      case "6":
         print("Goodbye....");
         return;
+
       default:
         print("Invalid choice");
     }
